312bd8409d7aaaab0c55c1889f8d15e019a4c3b7
yole <igbavagodswill4@gmail.com>
2023-12-21 00:04:26 +0000

    first commit, added all files to the repository

---
+++ script.js
@@ -0,0 +1,21 @@
+`use strict`;
+
+
+// toggling the navigation bar for mobile view
+const nav = document.querySelector('.nav');
+const mobileNav = document.querySelector('.mobileNav');
+const openNav = document.querySelector('.openNav');
+const closeNav = document.querySelector('.closeNav');
+
+
+
+//toggling the themes for the webpage
+const dayThemeActive = document.querySelector('.dayThemeActive');
+const nightThemeActive = document.querySelector('.nightThemeActive');
+
+const dayThemeInactive = document.querySelector('.dayThemeInactive');
+const nightThemeInactive = document.querySelector('.nightThemeInactive');
+
+//  <ion-icon name="sunny" class="dayThemeInactive"></ion-icon> 
+{/* <ion-icon name="moon" class="nightThemeInactive"></ion-icon> */}
+
