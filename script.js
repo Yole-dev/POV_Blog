fea74f73a61faba8c3256d99323c2cc803639471
yole <igbavagodswill4@gmail.com>
2023-12-22 11:57:57 +0000

    Finished creating the theme switch for the webpage

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 `use strict`;
 
+//body components
+const body = document.querySelector('body');
+
+
 
 // toggling the navigation bar for mobile view
 const nav = document.querySelector('.nav');
@@ -18,12 +22,31 @@ closeNav.addEventListener('click', function(){
 })
 
 //toggling the themes for the webpage
+const themeSwitch = document.querySelector('.themeSwitch')
+
 const dayThemeActive = document.querySelector('.dayThemeActive');
 const nightThemeActive = document.querySelector('.nightThemeActive');
 
 const dayThemeInactive = document.querySelector('.dayThemeInactive');
 const nightThemeInactive = document.querySelector('.nightThemeInactive');
 
-//  <ion-icon name="sunny" class="dayThemeInactive"></ion-icon> 
-{/* <ion-icon name="moon" class="nightThemeInactive"></ion-icon> */}
+const root = document.querySelector(':root');
 
+//  <ion-icon name="sunny" class="dayThemeInactive"></ion-icon> 
+//<ion-icon name="moon" class="nightThemeInactive"></ion-icon> 
+
+dayThemeActive.addEventListener('click', function(){
+    body.style.background = 'white';
+
+    root.style.setProperty('--themeBackground', 'white');
+    root.style.setProperty('--themeText', 'black');
+    root.style.setProperty('--shadow', '0px 0px 3px black');
+});
+
+nightThemeActive.addEventListener('click', function(){
+    body.style.background = 'black';
+
+    root.style.setProperty('--themeBackground', 'black');
+    root.style.setProperty('--themeText', 'white');
+    root.style.setProperty('--shadow', '0px 0px 3px white');
+});
